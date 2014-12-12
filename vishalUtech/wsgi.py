cd4f108d794beb6eeda899d9403a0910d59b6437
mateuszek <mateusz.dargacz@internetprofil.pl>
2014-12-12 18:11:48 +0100

    wsgi changes

--- vishalUtech/wsgi.py
+++ vishalUtech/wsgi.py
@@ -1,10 +1,14 @@
 from __future__ import unicode_literals
 
 import os
-
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-settings_module = "%s.settings" % PROJECT_ROOT.split(os.sep)[-1]
+from sys import path
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+PROJECT_ROOT = os.path.dirname(APP_ROOT)
+path.insert(0, PROJECT_ROOT)
+settings_module = "%s.settings" % APP_ROOT.split(os.sep)[-1]
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+
+
