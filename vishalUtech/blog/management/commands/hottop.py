ddf8869dc155f6038350a5cbe71bdcc7b757e40b
Bartlomiej Opajdowski <bopajdowski@gmail.com>
2014-12-17 16:24:09 +0100

    cron dla hottop

--- vishalUtech/blog/management/commands/hottop.py
+++ vishalUtech/blog/management/commands/hottop.py
@@ -6,22 +6,20 @@ __author__ = 'opel'
 from django.core.management.base import BaseCommand
 
 
+## CRON EXAMPLE (every 5 min)
+## */5 * * * * python manage.py hottop
+
 class Command(BaseCommand):
-    help = ("Updates views counter"
-    )
+    help = "Updates views counter"
 
     def handle(self, *apps, **options):
         blog_posts = BlogPost.objects.all()
         for bp in blog_posts:
             current = bp.views_count
             old = bp.views_count_delta
-            if old == 0:
-                old = bp.views_count
-            inc = current - old
-            print current, old, '(', inc, ')'
             bp.views_count_delta = current
             bp.save()
-        self.stdout.write("\nDone.\n")
+        self.stdout.write("\nSuccess.\n")
 
 
 
