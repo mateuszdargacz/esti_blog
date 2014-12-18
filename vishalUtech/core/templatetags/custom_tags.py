d3f188f8d7b4f7e6d71bc66fee08040e25764abd
Bartlomiej Opajdowski <bopajdowski@gmail.com>
2014-12-18 12:46:31 +0100

    rozwiazane problem z sitepermissions poprzez nadpisanie admin_dropdown_menu wlasnym templatetagiem

--- vishalUtech/core/templatetags/custom_tags.py
+++ vishalUtech/core/templatetags/custom_tags.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
-__author__ = 'mateusz'
+from mezzanine.core.templatetags.mezzanine_tags import admin_app_list
+from mezzanine.utils.sites import current_site_id
+
+from django.contrib.sites.models import Site
+
+__author__ = 'mateusz, opel'
 __date__ = '14.12.14 / 17:07'
 __git__ = 'https://github.com/mateuszdargacz'
 
@@ -7,3 +12,28 @@ from mezzanine import template
 
 
 register = template.Library()
+
+@register.inclusion_tag("admin/includes/dropdown_menu.html",
+                        takes_context=True)
+def own_admin_dropdown_menu(context):
+    """
+    OVERWRITES admin_dropdown_menu at mezzanine/core/templatetags/
+    Renders the app list for the admin dropdown menu navigation.
+    solves site permissions problem with /admin/ when you
+    are logged in as twitter user for example
+    """
+    context["dropdown_menu_app_list"] = admin_app_list(context["request"])
+    user = context["request"].user
+    sites = None
+    if user.is_superuser:
+        sites = Site.objects.all()
+    else:
+        try:
+            sites = user.sitepermissions.get().sites.all()
+        except:
+            context["dropdown_menu_sites"] = []
+            context["dropdown_menu_selected_site_id"] = current_site_id()
+            return context
+    context["dropdown_menu_sites"] = list(sites)
+    context["dropdown_menu_selected_site_id"] = current_site_id()
+    return context
