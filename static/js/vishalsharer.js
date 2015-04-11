b8247759fbe30f7140a42e7afe235447948437be
mateuszdargacz <mateuszdargacz@gmail.com>
2015-04-11 21:02:28 +0200

    dane , facebook

--- static/js/vishalsharer.js
+++ static/js/vishalsharer.js
@@ -1,10 +1,6 @@
 function mashFbSharrer(url, title, descr, image, winWidth, winHeight) {
 	var winTop = (screen.height / 2) - (winHeight / 2);
 	var winLeft = (screen.width / 2) - (winWidth / 2);
-    console.log('title', title);
-    console.log('descr', descr);
-    console.log('image', image);
-    console.log('winWidth', winWidth);
 	window.open('http://www.facebook.com/sharer.php?s=100&p[title]=' + title + '&p[summary]=' + descr + '&u=' + url + '&p[images][0]=' + image, 'sharer', 'top=' + winTop + ',left=' + winLeft + ',toolbar=0,status=0,width=' + winWidth + ',height=' + winHeight);
 }
 
